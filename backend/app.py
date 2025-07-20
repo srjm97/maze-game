b2c3a86c47e724ab549157a5222468266191ec5d
Sooraj Mathew <soorajsejal@gmail.com>
2025-07-20 22:53:50 +0530

    Few changes

--- backend/app.py
+++ backend/app.py
@@ -17,13 +17,23 @@ from models import Token, User, UserResponse
 from config import settings
 
 logger = logging.getLogger(__name__)
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await connect_to_mongo()
-    yield
-    await close_mongo_connection()
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     await connect_to_mongo()
+#     yield
+#     await close_mongo_connection()
 
-app = FastAPI(title="EchoMaze Backend", lifespan=lifespan)
+# app = FastAPI(title="EchoMaze Backend", lifespan=lifespan)
+
+app = FastAPI(title="EchoMaze Backend")
+
+@app.on_event("startup")
+async def startup_event():
+    await connect_to_mongo()
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await close_mongo_connection()
 
 # Configure CORS
 app.add_middleware(
@@ -268,8 +278,4 @@ async def handle_audio(file: UploadFile = File(...)):
 @app.get("/")
 def home():
     return {"message": "FastAPI is live on Vercel!"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
 
